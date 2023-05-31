1abdde81006f79220ec277cc8922a6ebda0f4f67
Kuntapat002 <mr.deadpool09@gmail.com>
2023-05-31 13:11:54 +0700

    edit3

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,42 @@
 import React from 'react'
 import Head from 'next/head'
-import 'bootstrap/dist/css/bootstrap.css';
 import Image from 'next/image';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 
 export default function index() {
   return (
     <>
 
-    <div clssname="text-center">
-      <Image src='/img.png' className='rounded' alt='img'width={200} height={200} />
-
-    </div>
-
     <Head>
       <title>Welcome to nextjs</title>
     </Head>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled">Disabled</a>
+      </div>
+    </div>
+
+  </div>
+</nav>
     
-   <div clssname="text-center"><button type="button" classname="btn btn-warning">Warning</button></div>
+<div className="text-center">
+      <Image src='/img.jpg' className='rounded' alt='img'width={200} height={200} />
+
+    </div>
+    
+   <div className="text-center"><button type="button" className="btn btn-warning">Warning</button></div>
 
 
     
