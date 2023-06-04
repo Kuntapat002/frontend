eafeb2e37e5c687586c6ebfdd652ba2beaeec732
Kuntapat002 <mr.deadpool09@gmail.com>
2023-06-04 22:00:45 +0700

    edit13

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function index() {
             <p className="text-body-secondary">Add some information about the album below, the author, or any other background context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off to some social networking sites or contact information.</p>
           </div>
           <div className="col-sm-4 offset-md-1 py-4">
-            <h4><a class="py-2 link-body-emphasis text-decoration-none" href="/contact.js">Contact</a></h4>
+            <h4><a class="py-2 link-body-emphasis text-decoration-none" href="contact">Contact</a></h4>
             <ul className="list-unstyled">
               <li><a href="#" className="text-white">Follow on Twitter</a></li>
               <li><a href="#" className="text-white">Like on Facebook</a></li>
@@ -34,7 +34,7 @@ export default function index() {
     </div>
     <div className="navbar navbar-dark bg-dark shadow-sm">
       <div className="container">
-        <a href="" className="navbar-brand d-flex align-items-center">
+        <a href="/." className="navbar-brand d-flex align-items-center">
           <svg href="index" width={20} height={20} fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} aria-hidden="true" className="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" /><circle cx={12} cy={13} r={4} /></svg>
           <strong>Fortune Express</strong>
         </a>
