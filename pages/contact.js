027867095dffe5d40fd4bcbdb876c60fc9d9cde6
Kuntapat002 <mr.deadpool09@gmail.com>
2023-06-07 08:38:10 +0700

    edit14.1

--- pages/contact.js
+++ pages/contact.js
@@ -54,7 +54,6 @@ export default function about() {
     <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
       <div className="feature col">
         <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-          <svg className="bi" width="1em" height="1em"><use xlinkHref="#collection" /></svg>
         </div>
         <h3 className="pb-2 border-bottom">Like on Facebook</h3>
         <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
@@ -65,7 +64,6 @@ export default function about() {
       </div>
       <div className="feature col">
         <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-          <svg className="bi" width="1em" height="1em"><use xlinkHref="Facebook" /></svg>
         </div>
         <h3 className="pb-2 border-bottom">Follow on Twitter</h3>
         <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
@@ -76,7 +74,6 @@ export default function about() {
       </div>
       <div className="feature col">
         <div className="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
-          <svg className="bi" width="1em" height="1em"><use xlinkHref="#toggles2" /></svg>
         </div>
         <h3 className="pb-2 border-bottom">Join on instagram</h3>
         <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
