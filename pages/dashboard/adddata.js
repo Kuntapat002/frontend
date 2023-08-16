09b7ba60c788d5cbe688305a9be6bd0f96cf4fbc
Kuntapat002 <mr.deadpool09@gmail.com>
2023-08-16 09:06:31 +0700

    edit25.2

--- pages/dashboard/adddata.js
+++ pages/dashboard/adddata.js
@@ -66,7 +66,7 @@ const handleSubmit = (event) => {
       <>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container">
-            <a className="navbar-brand" href="/">Fortune Express</a>
+            <Link className="navbar-brand" href="/">Fortune Express</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -136,17 +136,17 @@ const handleSubmit = (event) => {
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container">
-          <a className="navbar-brand" href="/">My App</a>
+          <Link className="navbar-brand" href="/">My App</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link" href="/">Home</a>
+                <Link className="nav-link" href="/">Home</Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/about">About</a>
+                <Link className="nav-link" href="/about">About</Link>
               </li>
             </ul>
             <div className="navbar-text ms-auto">
